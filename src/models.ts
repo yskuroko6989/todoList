68adbfb4a22abfb4f4f61afa575b85c591feadc0
Yudai Suzuki <kuroko0725cc@gmail.com>
2022-05-08 17:44:34 +0900

    new commit

---
+++ src/models.ts
@@ -0,0 +1,27 @@
+export interface Todo{
+  id: number;
+  todo: string;
+  isDone: boolean;
+}
+
+// type Actions =
+//   | {type: 'add',payload:string}
+//   | {type: 'remove',payload:number}
+//   | {type: 'done',payload:number};
+
+
+// const TodoReducer = (state: Todo[],action: Actions) => {
+
+// };
+
+// import React, { useReducer } from 'react'
+
+// const ReducerExample = () => {
+
+//   const [state,dispatch] = useReducer(TodoReducer,[])
+//   return (
+
+//   )
+// }
+
+// export default ReducerExample
